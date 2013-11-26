2a3b9082800b5f4d21b20e89c1c06cc3378180fe
Christian Ochsenkühn <christian.ochsenkuehn@gmail.com>
2013-11-26 16:54:27 +0100

    Added package controllers.acquisition Added URL /start-search to conf > routes

---
+++ BreakingNews/app/controllers/acquisition/Acquisition.java
@@ -0,0 +1,12 @@
+package controllers.acquisition;
+
+public class Acquisition {
+	
+	public Acquisition() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void startSearch() {
+		
+	}
+}
