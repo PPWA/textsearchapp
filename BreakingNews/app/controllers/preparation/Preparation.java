3eb853b246feb98754f086536a1a9e7c88c9b2db
Sebastian Mandel <smandel@hof-university.de>
2013-11-26 21:16:10 +0100

    New, old and similar topics/articles in package preparation.

--- BreakingNews/app/controllers/preparation/Preparation.java
+++ BreakingNews/app/controllers/preparation/Preparation.java
@@ -1,25 +1,124 @@
 package controllers.preparation;
 
+import java.util.List;
+import org.apache.lucene.document.Document;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class Preparation extends Controller{
+public class Preparation extends Controller {
 
-	public static Result getNewTopics(int offset) {
-		return ok("new"+offset);
+	public static Result getNewTopics(String offsetS) {
+		int offset;
+		ObjectNode response = Json.newObject();
+		ObjectNode result = Json.newObject();
+		ArrayNode articles = result.arrayNode();
+		Document document;
+		
+		try{
+			offset = Integer.parseInt(offsetS);
+		}
+		catch (Exception e)
+		{
+			return ok(index.render("Fehlerhafter Parameter"));
+		}
+		
+		List<Document> documents = Search.getDocumentsNew(offset);
+		for (int i = 0; i < documents.size(); i++) {
+			ObjectNode article = Json.newObject();
+			document = documents.get(i);
+			article.put("isNew", document.get("isNew"));
+			article.put("art_id", document.get("id"));
+			article.put("art_title", document.get("title"));
+			article.put("art_teaser", document.get("teaser"));
+			article.put("art_date", document.get("date"));
+			article.put("art_urlsource", document.get("urlsource"));
+			article.put("art_picture", document.get("picture"));
+			article.put("art_newportal", document.get("newsportal"));
+			article.put("art_topicHash", document.get("topicHash"));
+			article.put("art_explanation", document.get("explanation"));
+			articles.add(article);
+		}
+		response.put("articles", articles);
+		return ok(response);
 	}
 
-	public static Result getOldTopics(int offset) {
-		return ok("old" + offset);
+	public static Result getOldTopics(String offsetS) {
+		ObjectNode response = Json.newObject();
+		ObjectNode result = Json.newObject();
+		ArrayNode articles = result.arrayNode();
+		Document document;
+		int offset;
+		
+		try{
+			offset = Integer.parseInt(offsetS);
+		}
+		catch (Exception e)
+		{
+			return ok(index.render("Fehlerhafter Parameter"));
+		}
+		
+		List<Document> documents = Search.getDocumentsOld(offset);
+
+		for (int i = 0; i < documents.size(); i++) {
+			ObjectNode article = Json.newObject();
+			document = documents.get(i);
+			article.put("isNew", document.get("isNew"));
+			article.put("art_id", document.get("id"));
+			article.put("art_title", document.get("title"));
+			article.put("art_teaser", document.get("teaser"));
+			article.put("art_date", document.get("date"));
+			article.put("art_urlsource", document.get("urlsource"));
+			article.put("art_picture", document.get("picture"));
+			article.put("art_newportal", document.get("newsportal"));
+			article.put("art_topicHash", document.get("topicHash"));
+			articles.add(article);
+		}
+		response.put("articles", articles);
+		return ok(response);
 	}
-	
-	public static Result getSimilarArticles(int art_id) {
-		return ok("similar" + art_id);
+
+	public static Result getSimilarArticles(String topicHash) {
+		ObjectNode response = Json.newObject();
+		ObjectNode result = Json.newObject();
+		ArrayNode articles = result.arrayNode();
+		Document document;
+		List<Document> documents = Search.getSimilarDocuments(topicHash);
+		for (int i = 0; i < documents.size(); i++) {
+			ObjectNode article = Json.newObject();
+			document = documents.get(i);
+			article.put("art_id", document.get("id"));
+			article.put("art_title", document.get("title"));
+			article.put("art_teaser", document.get("teaser"));
+			article.put("art_date", document.get("date"));
+			article.put("art_urlsource", document.get("urlsource"));
+			article.put("art_picture", document.get("picture"));
+			article.put("art_newportal", document.get("newsportal"));
+			article.put("art_topichash", document.get("topicHash"));
+			articles.add(article);
+		}
+		response.put("articles", articles);
+		return ok(response);
 	}
-	
+
 	public static Result getNewsPortals() {
-		return ok("portals");
+		ObjectNode response = Json.newObject();
+		ObjectNode result = Json.newObject();
+		ArrayNode newsPortals = result.arrayNode();
+		Document portalLucene;
+		List<Document> newsPortalsLucene = Search.getNewsPortals();
+
+		for (int i = 0; i < newsPortalsLucene.size(); i++) {
+			ObjectNode portal = Json.newObject();
+			portalLucene = newsPortalsLucene.get(i);
+			portal.put("art_id", portalLucene.get("name"));
+			portal.put("art_title", portalLucene.get("count"));
+			newsPortals.add(portal);
+		}
+		response.put("portals", newsPortals);
+		return ok(response);
 	}
 }
