16255492084fc1f3cedf0d44bb8a8846821d4a51
Sebastian Mandel <smandel@hof-university.de>
2013-11-26 21:33:27 +0100

    Fix Akquisition.

--- BreakingNews/app/controllers/acquisition/Acquisition.java
+++ BreakingNews/app/controllers/acquisition/Acquisition.java
@@ -1,12 +1,14 @@
 package controllers.acquisition;
 
+import play.mvc.Result;
+
 public class Acquisition {
 	
 	public Acquisition() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static void startSearch() {
-		
+	public static Result startSearch() {
+		return null;
 	}
 }
