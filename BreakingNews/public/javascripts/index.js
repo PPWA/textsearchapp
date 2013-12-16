34bf232430869298bcc823f48aa6d462e3131b0b
anja <anja.ulrike.seidel@hof-university.de>
2013-12-16 10:08:33 +0100

    frontend

--- BreakingNews/public/javascripts/index.js
+++ BreakingNews/public/javascripts/index.js
@@ -2,6 +2,7 @@ init();
 refresh();
 portals();
 
+/*Initialisieren der aktuellen Artikel*/
 function init(){ 
 	$("#main").html("");	
 	$.getJSON("/new-topics", function (data){ 
@@ -9,10 +10,9 @@ function init(){
 
 			var curArticle = " ";
 			
-			curArticle = 				
-				
+			curArticle =				
 				'<div class="art_header">'+
-					'<a href="'+ item["art_urlsource"]+ '">' +
+					'<a href="'+ item["art_urlsource"]+ '" target="_new">' +
 						'<img class="img_rss" src="/assets/images/rss.png" alt="rss" />'+
 						'<h2>'+ item["art_title"] +'</h2>'+
 					'</a>'+
@@ -21,7 +21,7 @@ function init(){
 				'<img class="art_img" src="'+ item["art_urlpicture"] + '" alt="" />'
 				+ 
 				'<div class="art_main">'+
-					'<a href="'+ item["art_urlsource"]+ '" >'+'<p class="art_newsportal">'+ item["art_newportal"] +'</p>'+'</a>'+
+					'<a href="'+ item["art_urlsource"]+ '" target="_new">'+'<p class="art_newsportal">'+ item["art_newportal"] +'</p>'+'</a>'+
 					'<p class="teaser">'+ item["art_teaser"] +'</p>'+
 				'</div>'
 				+ 
@@ -29,40 +29,62 @@ function init(){
 				+
 				'<div style="clear:both;">'+'</div>';
 			
+			
 			$("#main").append("<article>" + curArticle + "</article>");
 
 			}); 
 	}); 
 	oldArticles();
+	
 }
 
-
+/*Anzeige Alte Artikel*/
 function oldArticles(){
-	$("#old_news").html("");
-	$.getJSON("/old-topics", function (data){ 
-		$.each(data.articles, function(i,item) { 
-		
-			var oldArticle = " ";
-			
-			oldArticle = 
-			
-				'<div class="old_art">'+
-					'<a href="'+ item["art_urlsource"]+ '">'+
-					'<img class="img_rss" src="/assets/images/rss.png" alt="" />'+
-					'<h2>'+ item["art_title"] +'</h2>'+
-					'</a>'+
-					'<p class="old_portal">'+ item["art_newportal"] +'</p>'+
-					'<p class="old_date">'+ item["art_date"]+ '<p>'+
-					'<div style="clear:both;"></div>'+
-				'</div>'+
-				'<hr />';		
+$.getJSON("/old-topics", function (data){ 
+	$.each(data.articles, function(i,item) { 
 	
-			$("#old_news").append(oldArticle);
+		var oldArticle = " ";
+		
+		oldArticle = 		
+			'<div class="old_art">'+
+				'<a href="'+ item["art_urlsource"]+ '" target="_new">'+
+				'<img class="img_rss" src="/assets/images/rss.png" alt="" />'+
+				'<h2>'+ item["art_title"] +'</h2>'+
+				'</a>'+
+				'<p class="old_portal">'+ item["art_newportal"] +'</p>'+
+				'<p class="old_date">'+ item["art_date"]+ '<p>'+
+				'<div style="clear:both;"></div>'+
+			'</div>'+
+			'<hr />';		
+
+		$("#old_news").append(oldArticle);
+
 		}); 
 	});
 }
 
-/*anfrage nach neuen Artikeln*/
+/*Ähnliche Artikel*/
+function similar(){
+	$.getJSON("/similar-articles", function (data){ 
+		$.each(data.articles, function(i,item) { 
+		
+			var simArticle = " ";
+			
+			simArticle = 
+				'<div class="art_similar">'+
+				'<a href="" target="_new">'+
+				'<p>'+'<img src="/assets/images/plus.png" alt="" />'+ '</p>'+'</a>'+'</div>';		
+			
+			alert("test");
+			
+			$("#main").append("<article>" + simArticle + "</article>");
+			
+
+			}); 
+		});	
+}
+
+/*Anfrage nach neuen Artikeln*/
 function refresh(){
 	$.getJSON("/start-search", function (data){ 
 		if (data.new_art_count != 0){
@@ -75,25 +97,23 @@ function refresh(){
 
 
 
-/*portals*/
+/*Statistik*/
 function portals(){
 	$.getJSON("/news-portals", function (data){ 
 		$.each(data.newsportals, function(i,item) { 
 			//alert("test");
 			var portals = " ";
-			var sum="";
-		
-		
-			portals = 
-				
-				'<div class="dia_name">' + item["np_name"] +'</div>' 
-				+
-				'<div class="dia_anzahl">' + item["np_count"] +'</div>'
-				+
-				'<br>';
-
-				
-
+					
+			portals =				
+			'<table id="bar">'+
+			    '<tbody>'+
+			        '<tr class="bar"><th>' + item["np_name"] +
+			        '</th><td><div style="width:' + item["np_count"] +'px;">&nbsp;</div>' + item["np_count"] +'</td></tr>'+
+			    '</tbody>'
+			+
+			'</table>';
+			
+			
 			$("#portals").append(portals);
 
 		}); 
